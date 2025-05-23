a985baa7c4dee6c11e2aa7817bd3d90e62d40f9e
Yuri <yurizetoles0123@gmail.com>
2025-05-22 20:23:17 -0400

    feat: adicionar lógica para armazenar e exibir pessoas no formulário

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,62 @@
+
 import './style.css'
 
 const nome = document.querySelector<HTMLInputElement>('#nome')!
 const email = document.querySelector<HTMLInputElement>('#email')!
 const formulario = document.querySelector<HTMLFormElement>('#formulario')!
 
-formulario.addEventListener('submit', (event) => {
-  event.preventDefault()
-  console.log(`Nome enviado: ${nome.value}`)
-  console.log(`Email enviado: ${email.value}`)
-  limparFormulario()
-})
+interface Pessoa {
+  nome: string
+  email: string
+  data: Date
+}
+const pessoas: Pessoa[] = []
 
 function limparFormulario() {
   formulario.reset()
   // nome.value = ''
   // email.value = ''
 }
+
+function sincronizar() {
+  localStorage.setItem('pessoas', JSON.stringify(pessoas))
+}
+
+function imprimirPessoas() {
+  const lista = document.querySelector<HTMLUListElement>('#lista')!
+  lista.innerHTML = ''
+  pessoas.forEach((pessoa) => {
+    const li = document.createElement('li')
+    li.textContent = `${pessoa.nome} - ${pessoa.email} - ${pessoa.data.toLocaleDateString()}`
+    lista.appendChild(li)
+  })
+}
+
+
+formulario.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const pessoa: Pessoa = {
+    nome: nome.value,
+    email: email.value,
+    data: new Date(),
+  }
+
+  pessoas.push(pessoa)
+
+  sincronizar()
+  imprimirPessoas()
+
+  limparFormulario()
+})
+
+const pessoasSalvas = localStorage.getItem('pessoas')
+if (pessoasSalvas) {
+  const pessoasParseadas: Pessoa[] = JSON.parse(pessoasSalvas)
+  pessoasParseadas.forEach((pessoa) => {
+    pessoas.push(pessoa)
+  })
+  imprimirPessoas()
+}
+// const pessoasSalvas = localStorage.getItem('pessoas')
 
